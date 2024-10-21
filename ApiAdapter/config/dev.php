965cdbd0bd979f4f47c57493520650f95f99ba4f
mykhailo.bakhmut <mykhailo.bakhmut.ca@gmail.com>
2024-10-20 22:24:22 -0400

    added config

---
+++ ApiAdapter/config/dev.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'tokenType'      => '',
+    'accessToken'    => '',
+    'refreshToken'   => '',
+    'externalApiUrl' => '',
+    'clientId'       => '',
+    'clientSecret'   => ''
+];
